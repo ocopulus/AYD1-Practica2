27d338ec4b7d0a9459acad16c33df9d46d3c8d37
luigitercero <luigitercero3@gmail.com>
2018-03-15 10:10:12 -0600

    agregando funcion Dividir

--- AYD1.java
+++ AYD1.java
@@ -21,5 +21,13 @@ public class AYD1 {
         String resultado = Integer.toString(c1, 2);
         return resultado;
     }
+
+    public String Dividir(String a, String b) {
+        int a1 = Integer.parseInt(a, 2);
+        int b1 = Integer.parseInt(b, 2);
+        int c1 = a1 / b1;
+        String resultado = Integer.toString(c1, 2);
+        return resultado;
+    }     
      
 }
