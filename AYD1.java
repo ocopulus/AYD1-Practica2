070389b614cea446497fbe5fa6ffd28be4988807
ocopulus <jlemus003@gmail.com>
2018-03-16 09:46:53 -0600

    a la funcion de multiplicacion no se valido nada

--- AYD1.java
+++ AYD1.java
@@ -38,6 +38,7 @@ public class AYD1 {
         String resultado = Integer.toString(c1, 2);
         return resultado;
     }
+    
     public String Potencia(String a,String b){
         int a1 = Integer.parseInt(a, 2);
         int b1 = Integer.parseInt(b, 2);
