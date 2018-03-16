12bedfe0daa283428c12f2442bdc209815169572
luigitercero <luigitercero3@gmail.com>
2018-03-16 10:57:37 -0600

    agregando try y catch a resta

--- AYD1.java
+++ AYD1.java
@@ -7,14 +7,7 @@ public class AYD1 {
     }
 	
 	public String Suma(String a, String b) {
-         int a1;
-         int b1'
-        try{
-            a1 = Integer.parseInt(a, 2);
-            b1 = Integer.parseInt(b, 2);
-        }catch (Exception e) {
-            return "";
-        }
+         
         int c1 = a1 + b1;
         
         String resultado = Integer.toString(c1, 2);
@@ -23,8 +16,14 @@ public class AYD1 {
     }
 
     public String Resta(String a, String b) {
-        int a1 = Integer.parseInt(a, 2);
-        int b1 = Integer.parseInt(b, 2);
+        int a1;
+         int b1'
+        try{
+            a1 = Integer.parseInt(a, 2);
+            b1 = Integer.parseInt(b, 2);
+        }catch (Exception e) {
+            return "";
+        }
         int c1 = a1 - b1;
         String resultado = Integer.toString(c1, 2);
         return resultado;
