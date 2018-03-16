75a084a3f07208718177c272fa590b2081d22090
ocopulus <jlemus003@gmail.com>
2018-03-16 09:43:28 -0600

    a la funcion resta no se valido nada

--- AYD1.java
+++ AYD1.java
@@ -14,7 +14,6 @@ public class AYD1 {
         return resultado;
     }
     
-
     public String Resta(String a, String b) {
         int a1 = Integer.parseInt(a, 2);
         int b1 = Integer.parseInt(b, 2);
